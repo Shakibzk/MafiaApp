9168edde791fec56cafc1405cb904fd4b568d94b
shakibzk <Schakibzk@gmail.com>
2025-09-01 14:54:12 +0200

    add frontend proxy

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins: [
+        // ... Deine Plugins
+    ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                // optional: changeOrigin: true
+            }
+        }
+    }
+})
