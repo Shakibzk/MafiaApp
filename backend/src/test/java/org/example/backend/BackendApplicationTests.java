a2c7743a4954e92f59d61bf5300eca61571cde32
shakibzk <Schakibzk@gmail.com>
2025-09-02 11:56:33 +0200

    update MongoDB and Sonar setup

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -3,11 +3,11 @@ package org.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
+
+
